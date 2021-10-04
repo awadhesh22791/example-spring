016df9fb77d71f91b9ca633b30cee348cd00a739
Awadhesh Kumar <awadhesh22791@gmail.com>
2021-10-04 12:27:27 +0530

    implemented mysql integration

--- core/src/main/java/org/jobrunr/examples/JobRunrStorageConfiguration.java
+++ core/src/main/java/org/jobrunr/examples/JobRunrStorageConfiguration.java
@@ -1,21 +1,24 @@
 package org.jobrunr.examples;
 
+import org.jobrunr.storage.StorageProvider;
+import org.jobrunr.storage.sql.mariadb.MariaDbStorageProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.sqlite.SQLiteDataSource;
 
 import java.nio.file.Paths;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackageClasses = JobRunrStorageConfiguration.class)
 public class JobRunrStorageConfiguration {
 
-    @Bean
+    /*@Bean
     public SQLiteDataSource dataSource() {
         final SQLiteDataSource dataSource = new SQLiteDataSource();
         dataSource.setUrl("jdbc:sqlite:" + Paths.get(System.getProperty("java.io.tmpdir"), "jobrunr-example.db"));
         return dataSource;
-    }
-
+    }*/
 }
